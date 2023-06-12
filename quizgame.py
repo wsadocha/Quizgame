9c4dc0e2831571ee5570a2897cedf3217c98f0bd
Wojtek <wojciechsadocha@gmail.com>
2023-06-12 22:22:09 +0200

    Function and difficulty levels added

--- quizgame.py
+++ quizgame.py
@@ -1,3 +1,5 @@
+from random import shuffle
+
 print("Welcome to my quiz about capital cities in Europe!")
 
 playing = input("Do you want to play? (yes/no) ")
@@ -6,10 +8,87 @@ if playing.lower() != "yes":
     print("Ok, bye!")
     quit()
 
-print("Ok, let's start!")
-score = 0
-questions = 5
+print("Choose the difficulty level:")
+level = input("Easy - press '1'\nMedium - press '2'\nHard - press '3'")
 
+easy = {
+    "poland": "warsaw",
+    "england": "london",
+    "france": "paris",
+    "spain": "madrid",
+    "germany": "berlin",
+    "italy": "rome",
+    "austria": "vienna",
+}
+medium = {
+    "bulgaria": "sofia",
+    "belarus": "minsk",
+    "croatia": "zagreb",
+    "finland": "helsinki",
+    "greece": "athens",
+    "norway": "oslo",
+    "sweden": "stockholm",
+}
+hard = {
+    "north macedonia": "skopje",
+    "moldova": "chisinau",
+    "malta": "valetta",
+    "liechtenstein": "vaduz",
+    "iceland": "reykjavik",
+    "estonia": "tallinn",
+    "albania": "tirana",
+}
+
+print("Ok, let's start!")
+# questions = 5
+
+
+def quiz(level):
+    score = 0
+    if level == '1':
+        # countries = shuffle(easy)
+        for country in easy:
+            print("What is the capital city of", country, "? ")
+            answer = input()
+            if answer.lower() == easy[country]:
+                print("Correct")
+                score += 1
+            else:
+                print("Incorrect")
+    elif level == '2':
+        for country in medium:
+            print("What is the capital city of", country, "? ")
+            answer = input()
+            if answer.lower() == medium[country]:
+                print("Correct")
+                score += 1
+            else:
+                print("Incorrect")
+    elif level == '3':
+        for country in hard:
+            print("What is the capital city of", country, "? ")
+            answer = input()
+            if answer.lower() == hard[country]:
+                print("Correct")
+                score += 1
+            else:
+                print("Incorrect")
+    result = (score/len(easy))*100
+    if result >= 90:
+        print("You're great! You've got " +
+              str(result)+"%"+" answers correct!")
+    elif 40 < result < 90:
+        print("You're not so bad. You've got " +
+              str(result)+"%"+" answers correct!")
+    else:
+        print("You need to practice! You've got only " +
+              str(result)+"%"+" answers correct!")
+
+
+quiz(level)
+
+
+"""
 answer = input("What is the capital city of Poland? ")
 if answer.lower() == "warsaw":
     print("Correct")
@@ -45,6 +124,7 @@ if answer.lower() == "oslo":
 else:
     print("Incorrect")
 
+
 result = (score/questions)*100
 
 if result >= 90:
@@ -56,3 +136,4 @@ elif 40 < result < 90:
 else:
     print("You need to practice! You've got only " +
           str(result)+"%"+" answers correct!")
+"""
