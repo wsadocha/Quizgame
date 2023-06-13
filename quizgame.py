fd00eda81da98f33216279c25b0088a386be53bd
Wojtek <wojciechsadocha@gmail.com>
2023-06-13 18:22:30 +0200

    Quizgame 2.0 Limit and shuffle dictionaries, possibility to play again

--- quizgame.py
+++ quizgame.py
@@ -2,138 +2,107 @@ from random import shuffle
 
 print("Welcome to my quiz about capital cities in Europe!")
 
-playing = input("Do you want to play? (yes/no) ")
+playing = input("Do you want to play? (y/n) ")
 
-if playing.lower() != "yes":
-    print("Ok, bye!")
-    quit()
+while playing.lower() == "y":
 
-print("Choose the difficulty level:")
-level = input("Easy - press '1'\nMedium - press '2'\nHard - press '3'")
+    print("Ok, let's start!")
+    print("Choose the difficulty level:")
+    level = input("Easy - press '1'\nMedium - press '2'\nHard - press '3'")
 
-easy = {
-    "poland": "warsaw",
-    "england": "london",
-    "france": "paris",
-    "spain": "madrid",
-    "germany": "berlin",
-    "italy": "rome",
-    "austria": "vienna",
-}
-medium = {
-    "bulgaria": "sofia",
-    "belarus": "minsk",
-    "croatia": "zagreb",
-    "finland": "helsinki",
-    "greece": "athens",
-    "norway": "oslo",
-    "sweden": "stockholm",
-}
-hard = {
-    "north macedonia": "skopje",
-    "moldova": "chisinau",
-    "malta": "valetta",
-    "liechtenstein": "vaduz",
-    "iceland": "reykjavik",
-    "estonia": "tallinn",
-    "albania": "tirana",
-}
+    easy = {
+        "poland": "warsaw",
+        "england": "london",
+        "france": "paris",
+        "spain": "madrid",
+        "germany": "berlin",
+        "italy": "rome",
+        "austria": "vienna",
+    }
+    medium = {
+        "bulgaria": "sofia",
+        "belarus": "minsk",
+        "croatia": "zagreb",
+        "finland": "helsinki",
+        "greece": "athens",
+        "norway": "oslo",
+        "sweden": "stockholm",
+    }
+    hard = {
+        "north macedonia": "skopje",
+        "moldova": "chisinau",
+        "malta": "valetta",
+        "liechtenstein": "vaduz",
+        "iceland": "reykjavik",
+        "estonia": "tallinn",
+        "albania": "tirana",
+    }
 
-print("Ok, let's start!")
-# questions = 5
+    def quiz(level):
+        score = 0
+        if level == '1':
+            c = list(easy.items())
+            shuffle(c)
+            countries = dict(c)
 
+            # a way to limit the number of elements in the dictionary
+            while len(countries) > 5:
+                countries.popitem()
 
-def quiz(level):
-    score = 0
-    if level == '1':
-        # countries = shuffle(easy)
-        for country in easy:
-            print("What is the capital city of", country, "? ")
-            answer = input()
-            if answer.lower() == easy[country]:
-                print("Correct")
-                score += 1
-            else:
-                print("Incorrect")
-    elif level == '2':
-        for country in medium:
-            print("What is the capital city of", country, "? ")
-            answer = input()
-            if answer.lower() == medium[country]:
-                print("Correct")
-                score += 1
-            else:
-                print("Incorrect")
-    elif level == '3':
-        for country in hard:
-            print("What is the capital city of", country, "? ")
-            answer = input()
-            if answer.lower() == hard[country]:
-                print("Correct")
-                score += 1
-            else:
-                print("Incorrect")
-    result = (score/len(easy))*100
-    if result >= 90:
-        print("You're great! You've got " +
-              str(result)+"%"+" answers correct!")
-    elif 40 < result < 90:
-        print("You're not so bad. You've got " +
-              str(result)+"%"+" answers correct!")
-    else:
-        print("You need to practice! You've got only " +
-              str(result)+"%"+" answers correct!")
+            for country in countries:
+                print("What is the capital city of", country, "? ")
+                answer = input()
+                if answer.lower() == countries[country]:
+                    print("Correct")
+                    score += 1
+                else:
+                    print("Incorrect")
+        elif level == '2':
 
+            c = list(medium.items())
+            shuffle(c)
+            countries = dict(c)
 
-quiz(level)
+            while len(countries) > 5:
+                countries.popitem()
 
+            for country in countries:
+                print("What is the capital city of", country, "? ")
+                answer = input()
+                if answer.lower() == countries[country]:
+                    print("Correct")
+                    score += 1
+                else:
+                    print("Incorrect")
+        elif level == '3':
 
-"""
-answer = input("What is the capital city of Poland? ")
-if answer.lower() == "warsaw":
-    print("Correct")
-    score += 1
-else:
-    print("Incorrect")
+            c = list(hard.items())
+            shuffle(c)
+            countries = dict(c)
 
-answer = input("What is the capital city of England? ")
-if answer.lower() == "london":
-    print("Correct")
-    score += 1
-else:
-    print("Incorrect")
+            while len(countries) > 5:
+                countries.popitem()
 
-answer = input("What is the capital city of France? ")
-if answer.lower() == "paris":
-    print("Correct")
-    score += 1
-else:
-    print("Incorrect")
+            for country in countries:
+                print("What is the capital city of", country, "? ")
+                answer = input()
+                if answer.lower() == countries[country]:
+                    print("Correct")
+                    score += 1
+                else:
+                    print("Incorrect")
+        result = (score/len(countries))*100
+        if result >= 80:
+            print("You're great! You've got " +
+                  str(result)+"%"+" answers correct!")
+        elif 40 < result < 80:
+            print("You're not so bad. You've got " +
+                  str(result)+"%"+" answers correct!")
+        else:
+            print("You need to practice! You've got only " +
+                  str(result)+"%"+" answers correct!")
 
-answer = input("What is the capital city of Italy? ")
-if answer.lower() == "rome":
-    print("Correct")
-    score += 1
-else:
-    print("Incorrect")
+    quiz(level)
+    playing = input("Do you want to play again? (y/n) ")
 
-answer = input("What is the capital city of Norway? ")
-if answer.lower() == "oslo":
-    print("Correct")
-    score += 1
-else:
-    print("Incorrect")
-
-
-result = (score/questions)*100
-
-if result >= 90:
-    print("You're great! You've got " +
-          str(result)+"%"+" answers correct!")
-elif 40 < result < 90:
-    print("You're not so bad. You've got " +
-          str(result)+"%"+" answers correct!")
-else:
-    print("You need to practice! You've got only " +
-          str(result)+"%"+" answers correct!")
-"""
+print("Ok, bye")
